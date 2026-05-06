90e96ba87160d68767fedb679acd2129cac226d7
songlin.chen <songlin.chen@pacston.com>
2026-05-06 14:57:46 +0800

    191. Number of 1 Bits - follow up: n & (n-1)

--- phase1/191_number_of_1_bits.cpp
+++ phase1/191_number_of_1_bits.cpp
@@ -28,16 +28,31 @@ public:
     }
 };
 
+class SolutionOptimized {
+    public:
+        // Follow up：n & (n-1) 消掉最右邊的 1
+        int hammingWeight(int n) {
+            int count = 0;
+            while (n) {
+                n &= (n - 1);
+                count++;
+            }
+            return count;
+        }
+};
+
 int main() {
     Solution sol;
+    SolutionOptimized sol_optimized;
 
     // Input: n = 11 (binary: 1011)
     // Output: 3
     cout << sol.hammingWeight(11) << "\n";
+    cout << sol_optimized.hammingWeight(11) << "\n";
 
     // Input: n = 128 (binary: 10000000)
     // Output: 1
     cout << sol.hammingWeight(128) << "\n";
-
+    cout << sol_optimized.hammingWeight(128) << "\n";
     return 0;
 }
