f0fc8cd4f3b5acc185f168501c4f804ebb003d59
songlin.chen <songlin.chen@pacston.com>
2026-05-07 10:39:49 +0800

    1. Two Sum

--- phase1/1_two_sum.cpp
+++ phase1/1_two_sum.cpp
@@ -25,7 +25,14 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
+        unordered_map<int, int> mp;
+        for(int i = 0;i<nums.size();i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+                return {mp[target-nums[i]],i};
+            }
+            mp[nums[i]]=i;
+        }
+        return {};
     }
 };
 
