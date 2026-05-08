0812f74e21fbff3627c0c3fbc2a60e4f6dc00659
songlin.chen <songlin.chen@pacston.com>
2026-05-08 16:38:29 +0800

    21. Merge Two Sorted Lists

--- phase2/21_merge_two_sorted_lists.cpp
+++ phase2/21_merge_two_sorted_lists.cpp
@@ -24,7 +24,15 @@ struct ListNode {
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
+        if(!list1) return list2;//如果list1為空，則返回list2
+        if(!list2) return list1;//如果list2為空，則返回list1
+        if(list1->val < list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);//遞歸檢查list1的下一個節點
+            return list1;
+        }else{
+            list2->next = mergeTwoLists(list1, list2->next);//遞歸檢查list2的下一個節點
+            return list2;
+        }
     }
 };
 
