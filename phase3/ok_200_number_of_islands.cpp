a2c39d549c37af021477eadfe30b530ea981e5a0
songlin.chen <songlin.chen@pacston.com>
2026-06-04 15:00:35 +0800

    200. Number of Islands

--- phase3/ok_200_number_of_islands.cpp
+++ phase3/ok_200_number_of_islands.cpp
@@ -20,7 +20,28 @@ using namespace std;
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        return {};
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    ans++;
+                    dfs(grid, i, j);//將島嶼淹沒
+                }
+            }   
+        }
+        return ans;
+    }
+    void dfs(vector<vector<char>>& grid, int i, int j){//i,j為當前位置,grid為地圖
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == '0'){//如果越界或為水，則返回
+            return;
+        }
+        grid[i][j] = '0';//將島嶼淹沒
+        dfs(grid, i+1, j);//下      
+        dfs(grid, i-1, j);//上
+        dfs(grid, i, j+1);//右
+        dfs(grid, i, j-1);//左
     }
 };
 
